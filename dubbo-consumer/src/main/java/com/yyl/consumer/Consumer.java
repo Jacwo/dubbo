cb1bfa0235a811e9fe5c79398b72e38bc1b55b61
杨园亮 <yangyuanliang@yangyuanliangdeMacBook-Pro.local>
2019-06-04 17:45:36 +0800

    优化

--- dubbo-consumer/src/main/java/com/yyl/consumer/Consumer.java
+++ dubbo-consumer/src/main/java/com/yyl/consumer/Consumer.java
@@ -8,7 +8,10 @@ public class Consumer {
     public static void main( String[] args ) {
         ClassPathXmlApplicationContext applicationContext=new ClassPathXmlApplicationContext("dubbo-consumer.xml");
         DemoService demoService = (DemoService) applicationContext.getBean("demoService");
-        demoService.sayHello("test");
+        while (true){
+            demoService.sayHello("test");
+
+        }
 
     }
 }
